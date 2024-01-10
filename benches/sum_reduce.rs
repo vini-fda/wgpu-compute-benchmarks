1834b6a02e6472fc242cefd5953ae1bb706bd358
Vinicius <vinifreitas.d.a@gmail.com>
2024-01-10 02:38:32 -0300

    added comparison to CPU & Burn

--- benches/sum_reduce.rs
+++ benches/sum_reduce.rs
@@ -1,8 +1,15 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::borrow::Borrow;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId, PlotConfiguration, AxisScale};
 use wgpu::*;
 use wgpu::util::DeviceExt;
 use wgpu_compute_benchmarks::kernels::kernel::Kernel;
 use wgpu_compute_benchmarks::kernels::sum_reduce::SumReduce;
+use burn::tensor::Tensor;
+use burn::backend::Wgpu;
+
+// Type alias for the backend to use.
+type Backend = Wgpu;
 
 const ERR_DID_NOT_FIND_ADAPTER: &str = "Failed to find an appropriate adapter";
 
@@ -43,47 +50,53 @@ async fn wgpu_init() -> (Device, Queue) {
 fn benchmark_gpu_1(c: &mut Criterion) {
     // Initialization
     let (device, queue) = pollster::block_on(wgpu_init());
+    const N: usize = 1_000;
+    let group_description = format!("SumReduce [executed {}x]", N);
+    let mut group = c.benchmark_group(&group_description);
 
-    let x = vec![1.0f32; 1 << 20];
+    for p in 20..=28 {
+        let xlen = 1 << p;
+        let x = vec![1.0f32; xlen];
 
-    let staging_buffer = device.create_buffer(&BufferDescriptor {
-        label: Some("Buffer for reading results"),
-        size: std::mem::size_of::<f32>() as BufferAddress,
-        usage: BufferUsages::MAP_READ | BufferUsages::COPY_DST,
-        mapped_at_creation: false,
-    });
-    let x_buffer = device.create_buffer_init(&util::BufferInitDescriptor {
-        label: Some("Input buffer"),
-        contents: bytemuck::cast_slice(&x),
-        usage: BufferUsages::STORAGE,
-    });
-    let tmp_buffer = device.create_buffer(&BufferDescriptor {
-        label: Some("Temporary buffer"),
-        size: x.len() as u64 * std::mem::size_of::<f32>() as u64,
-        usage: BufferUsages::STORAGE,
-        mapped_at_creation: false,
-    });
-    let output_buffer = device.create_buffer(&BufferDescriptor {
-        label: Some("Output buffer"),
-        size: std::mem::size_of::<f32>() as u64,
-        usage: BufferUsages::STORAGE | BufferUsages::COPY_SRC,
-        mapped_at_creation: false,
-    });
-
-    let sum_reduce = SumReduce::new(&device, &x_buffer, &tmp_buffer, &output_buffer);
-    const N: usize = 10_000;
-
-    let f = || {
-        // polls the function until it is ready
-        pollster::block_on(async {
-            execute_gpu_inner_1::<N>(&device, &queue,  &sum_reduce);
-            let mut result = [0.0f32];
-            get_data(&mut result, &output_buffer, &staging_buffer, &device, &queue).await;
-            result[0]
+        let staging_buffer = device.create_buffer(&BufferDescriptor {
+            label: Some("Buffer for reading results"),
+            size: std::mem::size_of::<f32>() as BufferAddress,
+            usage: BufferUsages::MAP_READ | BufferUsages::COPY_DST,
+            mapped_at_creation: false,
         });
-    };
-    let fn_description = format!("Parallel sum-reduce [executed {}x]", N);
-    c.bench_function(&fn_description, |b| b.iter(f));
+        let x_buffer = device.create_buffer_init(&util::BufferInitDescriptor {
+            label: Some("Input buffer"),
+            contents: bytemuck::cast_slice(&x),
+            usage: BufferUsages::STORAGE,
+        });
+        let tmp_buffer = device.create_buffer(&BufferDescriptor {
+            label: Some("Temporary buffer"),
+            size: x.len() as u64 * std::mem::size_of::<f32>() as u64,
+            usage: BufferUsages::STORAGE,
+            mapped_at_creation: false,
+        });
+        let output_buffer = device.create_buffer(&BufferDescriptor {
+            label: Some("Output buffer"),
+            size: std::mem::size_of::<f32>() as u64,
+            usage: BufferUsages::STORAGE | BufferUsages::COPY_SRC,
+            mapped_at_creation: false,
+        });
+    
+        let sum_reduce = SumReduce::new(&device, &x_buffer, &tmp_buffer, &output_buffer);
+    
+        let f = || {
+            // polls the function until it is ready
+            pollster::block_on(async {
+                execute_gpu_inner_1::<N>(&device, &queue,  &sum_reduce);
+                let mut result = [0.0f32];
+                get_data(&mut result, &output_buffer, &staging_buffer, &device, &queue).await;
+                result[0]
+            });
+        };
+        group.throughput(criterion::Throughput::Elements((N*xlen) as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(xlen), &(), |b, _| b.iter(f));
+    }
+    group.finish();
 }
 
 fn execute_gpu_inner_1<const N: usize>(
@@ -108,39 +121,63 @@ fn execute_gpu_inner_1<const N: usize>(
 fn benchmark_gpu_2(c: &mut Criterion) {
     // Initialization
     let (device, queue) = pollster::block_on(wgpu_init());
+    let mut group = c.benchmark_group("SumReduce [executed once]");
+    let plot_config = PlotConfiguration::default()
+        .summary_scale(AxisScale::Logarithmic);
+    group.plot_config(plot_config);
 
-    let x = vec![1.0f32; 1 << 20];
+    for p in 20..=28 {
+        let xlen = 1 << p;
+        let x = vec![1.0f32; xlen];
 
-    let staging_buffer = device.create_buffer(&BufferDescriptor {
-        label: Some("Buffer for reading results"),
-        size: std::mem::size_of::<f32>() as BufferAddress,
-        usage: BufferUsages::MAP_READ | BufferUsages::COPY_DST,
-        mapped_at_creation: false,
-    });
-    let x_buffer = device.create_buffer_init(&util::BufferInitDescriptor {
-        label: Some("Input buffer"),
-        contents: bytemuck::cast_slice(&x),
-        usage: BufferUsages::STORAGE,
-    });
-    let tmp_buffer = device.create_buffer(&BufferDescriptor {
-        label: Some("Temporary buffer"),
-        size: x.len() as u64 * std::mem::size_of::<f32>() as u64,
-        usage: BufferUsages::STORAGE,
-        mapped_at_creation: false,
-    });
-    let output_buffer = device.create_buffer(&BufferDescriptor {
-        label: Some("Output buffer"),
-        size: std::mem::size_of::<f32>() as u64,
-        usage: BufferUsages::STORAGE | BufferUsages::COPY_SRC,
-        mapped_at_creation: false,
-    });
+        let cpu_impl = || {
+            x.iter().sum::<f32>()
+        };
 
-    let f = || {
-        // polls the function until it is ready
-        pollster::block_on(execute_gpu_inner_2(&device, &queue, &staging_buffer, &x_buffer, &tmp_buffer, &output_buffer));
-    };
+        group.bench_with_input(BenchmarkId::new("CPU implementation", xlen), &(), |b, _| b.iter(cpu_impl));
 
-    c.bench_function("GPU implementation of parallel sum-reduce", |b| b.iter(f));
+        // GPU implementation
+        {
+            // we put this in its own scope so that the buffers are dropped before we run the Burn implementation
+            let staging_buffer = device.create_buffer(&BufferDescriptor {
+                label: Some("Buffer for reading results"),
+                size: std::mem::size_of::<f32>() as BufferAddress,
+                usage: BufferUsages::MAP_READ | BufferUsages::COPY_DST,
+                mapped_at_creation: false,
+            });
+            let x_buffer = device.create_buffer_init(&util::BufferInitDescriptor {
+                label: Some("Input buffer"),
+                contents: bytemuck::cast_slice(&x),
+                usage: BufferUsages::STORAGE,
+            });
+            let tmp_buffer = device.create_buffer(&BufferDescriptor {
+                label: Some("Temporary buffer"),
+                size: x.len() as u64 * std::mem::size_of::<f32>() as u64,
+                usage: BufferUsages::STORAGE,
+                mapped_at_creation: false,
+            });
+            let output_buffer = device.create_buffer(&BufferDescriptor {
+                label: Some("Output buffer"),
+                size: std::mem::size_of::<f32>() as u64,
+                usage: BufferUsages::STORAGE | BufferUsages::COPY_SRC,
+                mapped_at_creation: false,
+            });
+            let f = || {
+                pollster::block_on(execute_gpu_inner_2(&device, &queue, &staging_buffer, &x_buffer, &tmp_buffer, &output_buffer));
+            };
+
+            group.bench_with_input(BenchmarkId::new("GPU implementation", xlen), &(), |b, _| b.iter(f));
+        }
+
+        // implementation using burn
+        let xt = Tensor::<Backend, 1>::from_data(x.as_slice());
+        let burn_impl = || {
+            xt.clone().sum()
+        };
+        
+        group.bench_with_input(BenchmarkId::new("Burn implementation", xlen), &(), |b, _| b.iter(burn_impl));
+    }
+    group.finish();
 }
 
 async fn execute_gpu_inner_2(
@@ -198,5 +235,5 @@ async fn get_data<T: bytemuck::Pod>(
     staging_buffer.unmap();
 }
 
-criterion_group!(benches, benchmark_gpu_1, benchmark_gpu_2);
+criterion_group!(benches, benchmark_gpu_2);
 criterion_main!(benches);
